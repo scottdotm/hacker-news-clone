61935192545d5f7a0d2cb7cf8dacb63a5fb89c6e
Scottdotm <scottdotm@gmail.com>
2024-09-27 17:15:23 -0500

    Added additional pages to scaffolding of website.

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import api from "./api/api"
+export default function login() {
+    return (
+        <div>
+            <h1>Login Page - Users login here</h1>
+            {api}
+        </div>
+    )
+}
